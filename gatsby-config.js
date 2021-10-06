f3f1e8cbce4330e1f9db7717a0dd317060b38f04
Nelson Ochoa <nelsonochoam@Raquels-MacBook-Air.local>
2021-10-06 05:52:22 -0600

    site name change :)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Dhandho`,
+    title: `Nelson Ochoa`,
     author: {
       name: `Nelson Ochoa`,
       summary: `We are what we repeatedly do. Excellence then, is not an act, but a habit. - Aristotle`,
